c4662a42dd27d67d43769cab1b8fb00bfe933f88
sinius15 <sinius15@gmail.com>
2013-09-04 15:55:59 +0200

    rec folder with pictures and example level
    WORKING BULD

---
+++ src/sinius/maze/drawing/Finish.java
@@ -0,0 +1,28 @@
+package sinius.maze.drawing;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+import sinius.maze.Game;
+
+public class Finish {
+
+	static Color gray = new Color(0f, 0f, 0f, 0.85f);
+	static Font font = new Font("Zolano Serif BTN", Font.PLAIN, 25);
+	
+	public static void Draw(Graphics2D g){
+		g.setColor(gray);
+		g.fillRect(0, 0, 800, 800);	
+		g.setColor(Color.white);
+		g.setFont(font);
+		
+		g.drawString("Congratulations!", 200, 200);
+		g.drawString("You finished this maze.", 200, 230);
+		
+		g.drawString("Press the X to exit!", 200, 260);
+		g.drawString("Time: " + Game.timer.getTime(), 200, 300);
+		
+	}
+	
+}
