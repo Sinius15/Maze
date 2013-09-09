277ad35938c9db84c4c0bf691d7a321a33d10ab5
sinius15 <sinius15@gmail.com>
2013-09-09 17:03:58 +0200

    Broken Code

--- src/sinius/maze/graphicsLayer/Entitys.java
+++ src/sinius/maze/graphicsLayer/Entitys.java
@@ -1,8 +1,10 @@
 package sinius.maze.graphicsLayer;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 
+import sinius.maze.Entity;
 import sinius.maze.Game;
 import sinius.maze.MainProgram;
 import sinius.maze.GraphicsLayer;
@@ -18,7 +20,21 @@ public class Entitys implements GraphicsLayer{
 	@Override
 	public void Draw(Graphics2D graphics) {
 		
-		Game.level.editEntitys("draw", graphics, null, null, 0, 0, null);
+		//Game.level.editEntitys("draw", graphics, null, null, 0, 0, null);
+		
+		for(Entity f : Game.level.getEntitys()){
+			if(f.onGrid())
+				graphics.drawImage(f.getFont(), f.getX()*Game.ppb_x, f.getY()*Game.ppb_y, Game.ppb_x, Game.ppb_y, null);
+			else{
+				if(f.getSize() != null)
+					graphics.drawImage(f.getFont(), f.getX(), f.getY(), f.getSize().width, f.getSize().height, null);
+				else
+					graphics.drawImage(f.getFont(), f.getX(), f.getY(), Game.ppb_x, Game.ppb_y, null);
+			}
+			graphics.setColor(Color.black);
+			f.advancedRender(graphics, Game.editMode);
+				
+		}
 		
 		if(!Game.editMode){
 			Player p = MainProgram.game.getPlayer();
